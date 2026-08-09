f3ee87d11a0f89970a3c5acbe43dd30b2c3b0610
Zhustas <zhongli3137@gmail.com>
2026-08-09 19:19:03 +0300

    Fix position

--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ const { LogicalPosition, LogicalSize } = window.__TAURI__.dpi;
 const monitor = await currentMonitor();
 const currentWindow = await getCurrentWindow();
 
-const outerSize = await currentWindow.outerSize();
-
-const WINDOW_WIDTH_PERCENTAGE = 0.16, // 0.15
-    WINDOW_HEIGHT_PERCENTAGE = 0.2; // 0.18
-
-const WINDOW_WIDTH = monitor.size.width * WINDOW_WIDTH_PERCENTAGE,
-    WINDOW_HEIGHT = monitor.size.height * WINDOW_HEIGHT_PERCENTAGE,
-    PADDING = 5;
-
-await currentWindow.setSize(new LogicalSize(WINDOW_WIDTH, WINDOW_HEIGHT));
-
-const OUTER_WIDTH = outerSize.width - WINDOW_WIDTH;
-const MONITOR_RATIO = monitor.size.width / monitor.size.height;
-
 const exitBtn = document.querySelector('#exit');
 exitBtn.addEventListener('click', async function () {
     currentWindow.destroy();
 });
+
+const WINDOW_WIDTH_PERCENTAGE = 0.17, // 0.15
+    WINDOW_HEIGHT_PERCENTAGE = 0.2, // 0.18
+    PADDING_PERCENT = 0.002;
+
+const WINDOW_WIDTH = monitor.size.width * WINDOW_WIDTH_PERCENTAGE,
+    WINDOW_HEIGHT = monitor.size.height * WINDOW_HEIGHT_PERCENTAGE,
+    PADDING = monitor.size.width * PADDING_PERCENT;
+
+await currentWindow.setSize(new LogicalSize(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+const outerSize = await currentWindow.outerSize();
+
+const OUTER_WIDTH = outerSize.width - WINDOW_WIDTH;
 
 await currentWindow.setPosition(
     new LogicalPosition(
