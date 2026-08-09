77d7c1e82fdf4d019d29096ed7eac7576019461d
Zhustas <zhongli3137@gmail.com>
2026-08-09 18:23:34 +0300

    Refactor time handling and refactor to set window size by percentages

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ const GTA_VI = new Date(2026, 10, 19);
 
 const countdownP = document.querySelector('#countdown');
 
-function updateCountdown(date) {
-    const days = Math.round(date.getTime() / 1000 / 60 / 60 / 24);
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
+function updateCountdown(toWait) {
+    const days = toDisplayValue(toWait / 1000 / 60 / 60 / 24);
+    const hours = toDisplayValue((toWait / 1000 / 60 / 60) % 24);
+    const minutes = toDisplayValue((toWait / 1000 / 60) % 60);
+    const seconds = toDisplayValue((toWait / 1000) % 60);
 
     countdownP.textContent = `${days} d. ${hours}:${minutes}:${seconds}`;
 }
 
-function getLocalDate(date) {
-    const hours = 3 * 1000 * 60 * 60;
+function pad(value) {
+    return value.toString().padStart(2, '0');
+}
 
-    return new Date(date.getTime() - hours);
+function toDisplayValue(value) {
+    return pad(Math.round(value));
 }
 
 setInterval(() => {
     const date = new Date();
-    const toWaitDate = new Date(GTA_VI.getTime() - date.getTime());
+    const toWait = GTA_VI.getTime() - date.getTime();
 
-    updateCountdown(getLocalDate(toWaitDate));
+    updateCountdown(toWait);
 }, 1000);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
-const { getCurrentWindow } = window.__TAURI__.window;
+const { getCurrentWindow, currentMonitor } = window.__TAURI__.window;
+const { LogicalPosition, LogicalSize } = window.__TAURI__.dpi;
+
+const monitor = await currentMonitor();
+const currentWindow = await getCurrentWindow();
+
+const outerSize = await currentWindow.outerSize();
+
+const WINDOW_WIDTH_PERCENTAGE = 0.16, // 0.15
+    WINDOW_HEIGHT_PERCENTAGE = 0.2; // 0.18
+
+const WINDOW_WIDTH = monitor.size.width * WINDOW_WIDTH_PERCENTAGE,
+    WINDOW_HEIGHT = monitor.size.height * WINDOW_HEIGHT_PERCENTAGE,
+    PADDING = 5;
+
+await currentWindow.setSize(new LogicalSize(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+const OUTER_WIDTH = outerSize.width - WINDOW_WIDTH;
+const MONITOR_RATIO = monitor.size.width / monitor.size.height;
 
 const exitBtn = document.querySelector('#exit');
-
 exitBtn.addEventListener('click', async function () {
-    await getCurrentWindow().destroy();
+    currentWindow.destroy();
 });
+
+await currentWindow.setPosition(
+    new LogicalPosition(
+        monitor.size.width - OUTER_WIDTH / 2 - WINDOW_WIDTH - PADDING,
+        PADDING,
+    ),
+);
